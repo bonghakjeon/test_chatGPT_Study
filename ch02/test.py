66785f74eb191ef5c5ae99ff42726e2bdb615c18
DESKTOP-5NUN1M7\bhjeon <bhjeon@imbu.co.kr>
2024-12-11 17:48:03 +0900

    인프런 초보자를 위한 ChatGPT API 활용법 - API 기본 문법부터 12가지 프로그램 제작 배포까지 Ch02 - ChatGPT API 사용법 익히기 5강 - OpenAI API 설명 및 API 키 발급

---
+++ ch02/test.py
@@ -0,0 +1,16 @@
+# OpenAI API 키(key) 발급 사이트 
+# 참고 URL - https://platform.openai.com/
+
+# OpenAI API 키(key) 발급 계정 
+# 아이디 : wjsqhd0827@gmail.com
+
+# OpenAI API 키(key) 값
+# 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
+import matplotlib.pyplot as plt
+
+print(1 + 1)
+print('여기에 내가 만들고 싶은 것을 적어보세요')
+
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
