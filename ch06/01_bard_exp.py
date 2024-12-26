3c0904e411a0c9cfb634c79c477af12cb6b8bfe6
DESKTOP-5NUN1M7\bhjeon <bhjeon@imbu.co.kr>
2024-12-26 15:52:29 +0900

    인프런 초보자를 위한 ChatGPT API 활용법 - API 기본 문법부터 12가지 프로그램 제작 배포까지 Ch06 - ChatGPT vs Bard 답변 비교 프로그램 24강 - Bard API 사용방법 익히기

--- ch06/01_bard_exp.py
+++ ch06/01_bard_exp.py
@@ -2,3 +2,13 @@
 # streamlit 터미널 실행 명령어
 # streamlit run 01_bard_exp.py
 
+from gemini import Gemini
+
+cookies = {
+    "__Secure-1PSID" : "g.a000rghfMQ2AKm7_0LcfWGnUAncDNJettjWP63J9HCrW4-7nF0MVzVzlaNQ3_ahCliCRiVnYTgACgYKAd8SARESFQHGX2Miu_7udkIg-n3tHty5AyB4rBoVAUF8yKqXxFjSPwkeHlqfORCrSjEx0076"
+}
+# client = Gemini(auto_cookies=True)
+client = Gemini(cookies=cookies, timeout=30)
+
+response = client.generate_content("LG 트윈스에 대해 설명해줘")
+print(response.payload)
