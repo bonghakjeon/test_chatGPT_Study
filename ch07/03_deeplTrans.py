afc21306743132694de67fb197e32003070570e1
DESKTOP-5NUN1M7\bhjeon <bhjeon@imbu.co.kr>
2025-01-02 15:32:31 +0900

    인프런 초보자를 위한 ChatGPT API 활용법 - API 기본 문법부터 12가지 프로그램 제작 배포까지 Ch07 - 번역 플랫폼 비교 프로그램 만들기 30강 - Deepl 번역기 API 사용방법 익히기

---
+++ ch07/03_deeplTrans.py
@@ -0,0 +1,13 @@
+# TODO : DeepL API 신용카드 등록 오류로 인하여 DeepL API 키 발급 실패함. (2025.01.02 jbh)
+# DeepL 파이썬 오픈 소스 패키지 deepl 
+# 터미널 설치 명령어
+# pip install deepl==1.15.0
+import deepl
+
+auth_key = "API Key"  # Replace with your key
+translator = deepl.Translator(auth_key)
+
+text ="GPT-4 is more creative and collaborative than ever before. It can generate, edit, and iterate with users on creative and technical writing tasks, such as composing songs, writing screenplays, or learning a user??s writing style."
+
+result = translator.translate_text(text, target_lang="KO")
+print(result.text)
