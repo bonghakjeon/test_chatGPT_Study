803114a254ffb0a50d157eede27cd2f06c85b94f
DESKTOP-5NUN1M7\bhjeon <bhjeon@imbu.co.kr>
2025-02-03 17:54:33 +0900

    테스트 20

---
+++ ch12/03_telegramAPI_exp1.py
@@ -0,0 +1,22 @@
+# 파이썬 파일 "03_telegramAPI_exp1.py" 터미널 실행 명령어
+# python 03_telegramAPI_exp1.py
+
+# 파이썬 파일 "03_telegramAPI_exp1.py" 실행시 
+# 오류 메시지 ModuleNotFoundError: No module named 'urllib3'
+# 해결하기 위해 파이썬 기본 내장 패키지(라이브러리) - "urllib3" 삭제 및 
+# 버전 1.26.18 설치 진행 
+# 참고 URL - https://heidong.tistory.com/278
+# 1) pip uninstall urllib3
+# 2) pip install urllib3==1.26.18
+
+# 텔레그램 PC 프로그램 다운로드 방법
+# 참고 URL - https://m.blog.naver.com/voramines/223163473669
+
+# 텔레그램 PC 프로그램 다운로드 사이트
+# 다운로드 URL - https://desktop.telegram.org/
+
+# 텔레그램 모바일 어플 언어 "한국어" 변경 방법
+# 참고 URL - https://lifenourish.tistory.com/2165
+
+# JSON 데이터 보기 편하게 해주는 jsonformatter 사이트 
+# 참고 URL - https://jsonformatter.curiousconcept.com/
